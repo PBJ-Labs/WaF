b4ae62155ccfc225117fd27f20c64e4f4a66084b
PB&J(*this)-sandwich <140750964+PBJ-Labs@users.noreply.github.com>
2025-03-07 22:39:00 +0000

    gsrc cpp cxx

---
+++ game/gsrc-1.0/gsrc.cpp
@@ -0,0 +1,8 @@
+#include <gsxccsrc>
+
+const char * GsrcExtensionParser(const extension * gsrcFile){
+        if( GsrcExtensionParser ){
+            gsrcFile == "*.gsrc" && "*.cpp";
+        }
+  return ".gsrc";
+};
---
+++ game/gsrc-1.0/gxcclanger@$netpckstr@vslib81.hpp
@@ -0,0 +1,46 @@
+#include "vlibafx.h"
+
+#ifndef GSRC_GSTD_LIB_HPP
+#define GSRC_GSTD_LIB_HPP
+
+#ifndef gxcc
+#define gxcc template
+#endif
+
+#ifndef scripttpcc
+#define scripttpcc typename
+#endif
+
+#ifndef version
+#define version double
+#endif
+
+#ifndef treesrc
+#define treesrc std::string
+#endif
+
+#ifndef extension
+#define extension const char
+#endif
+
+#ifndef thread
+#define thread void
+#endif
+
+#ifndef script
+#define script class 
+#endif
+
+#ifndef gameasm
+#define gasmxcc public
+#endif
+
+#ifndef gamescriptspace
+#define gamescriptspace namespace
+#endif
+
+#ifndef gsrc
+#define gsrc gxcclanger@$netpckstr@vslib81.hpp
+#endif
+
+#endif
